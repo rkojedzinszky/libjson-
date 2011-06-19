54e07f623e8da45d5f331fd770c97d2f9dbf2683
Richard Kojedzinszky <krichy@cflinux.hu>
2011-06-19 22:24:43 +0200

    Removed .ex and .EX files from debian/

--- debian/libjson.cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the libjson package
-#
-0 4	* * *	root	[ -x /usr/bin/libjson_maintenance ] && /usr/bin/libjson_maintenance
--- debian/shlibs.local.ex
+++
@@ -1,1 +0,0 @@
-liblibjson 0.1 libjson (>> 0.1-0), libjson (<< 0.1-99)
